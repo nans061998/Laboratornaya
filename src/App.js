bd5b226a58c385248a195e76f3ef9e224f0211f4
Анастасия <nastyaoseeva1998@gmail.com>
2021-01-11 20:53:08 +0700

    Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App =()=>{
+  return(
+    <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-6 text-center">
+            <div className="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative" style={{padding:"25px",margin:"25px"}}>
+              <form>
+                <div class="form-group" className="text-left">
+                  <h4>Events in San Francisco</h4>
+                  <label for="StartDate" >Start date</label>
+                  <input type="date" className="form-control" id="StartDate"/>
+                </div>
+                <br/>
+                <div class="form-group" className="text-left">
+                  <label for="EndDate">End date</label>
+                  <input type="date" className="form-control" id="EndDate"/>
+                </div>
+                <br/>
+                <div class="form-group" className="text-left">
+                  <label for="Filter">Filtration by keywords</label>
+                  <input type="text" className="form-control" id="Filter"/>
+                </div>
+                <br/>
+                <button type="submit" className="btn btn-primary">Get events</button>
+              </form>  
+            </div>
+          </div>
+        </div>
+      </div>
+  )
 }
-
+  
 export default App;
